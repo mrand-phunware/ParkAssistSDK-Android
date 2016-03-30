412b5df8876a18e68636cd6ef1f210b2f9f11762
Molly Rand <mrand@phunware.com>
2016-03-30 10:19:52 -0500

    javadoc comments

--- sdk/src/main/java/com/phunware/parkassist/networking/ParkAssistNetworkingInterface.java
+++ sdk/src/main/java/com/phunware/parkassist/networking/ParkAssistNetworkingInterface.java
@@ -5,15 +5,28 @@ import android.graphics.Bitmap;
 import org.json.JSONArray;
 
 /**
- * Created by mrand on 3/29/16.
+ * Whatever networking library you choose to use should be able to conform to these method calls
  */
 public interface ParkAssistNetworkingInterface {
 
+    /**
+     * retrieve JSON response array from ParkAssist server
+     * @param URL relative URL path String
+     * @param responseHandler capable of passing along success data or failure throwable
+     */
     void getJSON(String URL, ParkAssistJSONResponseInterface responseHandler);
 
+    /**
+     * retrieve image response from ParkAssist server
+     * @param URL relative URL path String
+     * @param responseHandler capable of passing along success data or failure throwable
+     */
     void getImage(String URL, ParkAssistImageResponseInterface responseHandler);
 
 
+    /**
+     * interface for passing server data back to the application & processing into convenience objects
+     */
     interface ParkAssistJSONResponseInterface {
 
         void onSuccess(JSONArray response);
@@ -21,6 +34,9 @@ public interface ParkAssistNetworkingInterface {
         void onFailure(Throwable e);
     }
 
+    /**
+     * interface for passing Bitmaps received from the server back to the application layer
+     */
     interface ParkAssistImageResponseInterface {
         void onSuccess(Bitmap image);
 
