932894d79c1f17bd4b6891c9abd73b1c55f35e9f
Molly Rand <mrand@phunware.com>
2016-04-04 15:12:32 -0500

    ui test and everything

--- parkassistdemo/src/androidTest/java/com/phunware/parkassistdemo/ApplicationTest.java
+++ parkassistdemo/src/androidTest/java/com/phunware/parkassistdemo/ApplicationTest.java
@@ -1,13 +1,32 @@
 package com.phunware.parkassistdemo;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void testSearchTextDoesntDelete() {
+        onView(withId(R.id.plate_input))
+                .perform(typeText("ZZZ"), closeSoftKeyboard());
+        onView(withId(R.id.submit_search_button)).perform(click());
+        onView(withId(R.id.plate_input))
+                .check(matches(withText("ZZZ")));
     }
+
 }
